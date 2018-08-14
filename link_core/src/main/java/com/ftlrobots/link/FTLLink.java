77acc5e176c38989f89e6f6a29f5c8929f211d79
ZQ Yeo <zhiquan.yeo@gmail.com>
2018-08-14 17:14:07 -0400

    WIP: Move logic to FTLLink

--- link_core/src/main/java/com/ftlrobots/link/FTLLink.java
+++ link_core/src/main/java/com/ftlrobots/link/FTLLink.java
@@ -1,7 +1,10 @@
 package com.ftlrobots.link;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import com.ftlrobots.link.LinkCoreConstants.RobotMode;
 import com.ftlrobots.link.data.JoystickData;
@@ -9,23 +12,194 @@ import com.ftlrobots.link.data.MatchInfo;
 
 public abstract class FTLLink {
 
+    private Map<Integer, Boolean> mDigitalOutputValues = new HashMap<>();
+    private Map<Integer, Double> mPWMOutputValues = new HashMap<>();
+    private Map<Integer, Boolean> mDigitalInputValues = new HashMap<>();
+    private Map<Integer, Double> mAnalogInputValues = new HashMap<>();
+
+    private boolean mRobotDisabled = true;
+    private RobotMode mRobotMode = RobotMode.Autonomous;
+    private MatchInfo mMatchInfo;
+    private JoystickData[] mJoystickData;
+
+    private List<ILinkListener> mListeners = new ArrayList<>();
+
     public abstract void start();
 
+    // =======================================================
+    // LISTENER REGISTRATION
+    // =======================================================
+
+    public void registerListener(ILinkListener listener) {
+        mListeners.add(listener);
+    }
+
+    public void unregisterListener(ILinkListener listener) {
+        mListeners.remove(listener);
+    }
+
+    // =======================================================
+    // PUBLIC INTERFACES TO DATA
+    //========================================================
+
     // System Controller
-    public abstract boolean getRobotDisabled();
-    public abstract RobotMode getRobotMode();
-    public abstract MatchInfo getMatchInfo();
+    public synchronized boolean getRobotDisabled() {
+        return mRobotDisabled;
+    }
+
+    public synchronized RobotMode getRobotMode() {
+        return mRobotMode;
+    }
+
+    public synchronized MatchInfo getMatchInfo() {
+        return mMatchInfo;
+    }
+
+    public synchronized JoystickData[] getJoystickData() {
+        return mJoystickData;
+    }
+
     public abstract double getMatchTime();
-    public abstract JoystickData[] getJoystickData();
 
     // Hardware Interface
-    public abstract void setDigitalOutput(int port, boolean value);
-    public abstract void setDigitalOutput(Map<Integer, Boolean> digitalValueMap);
-    public abstract void setPWMOutput(int port, double value);
-    public abstract void setPWMOutput(Map<Integer, Double> pwmValueMap);
+    public synchronized void setDigitalOutput(int port, boolean value) {
+        mDigitalOutputValues.put(port, value);
+        onDigitalOutputValuesChanged();
+    }
 
-    public abstract boolean getDigitalInput(int port);
-    public abstract Map<Integer, Boolean> getDigitalInputMulti(List<Integer> portList);
-    public abstract double getAnalogInput(int port);
-    public abstract Map<Integer, Double> getAnalogInputMulti(List<Integer> portList);
+    public synchronized void setDigitalOutput(Map<Integer, Boolean> digitalValueMap) {
+        for (Entry<Integer, Boolean> valEntry : digitalValueMap.entrySet()) {
+            mDigitalOutputValues.put(valEntry.getKey(), valEntry.getValue());
+        }
+        onDigitalOutputValuesChanged();
+    }
+
+    public synchronized void setPWMOutput(int port, double value) {
+        mPWMOutputValues.put(port, value);
+        onPWMOutputValuesChanged();
+    }
+
+    public synchronized void setPWMOutput(Map<Integer, Double> pwmValueMap) {
+        for (Entry<Integer, Double> valEntry : pwmValueMap.entrySet()) {
+            mPWMOutputValues.put(valEntry.getKey(), valEntry.getValue());
+        }
+        onPWMOutputValuesChanged();
+    }
+
+    public synchronized boolean getDigitalInput(int port) {
+        if (!mDigitalInputValues.containsKey(port)) {
+            return false;
+        }
+        return mDigitalInputValues.get(port);
+    }
+
+    public synchronized Map<Integer, Boolean> getDigitalInputMulti(List<Integer> portList) {
+        Map<Integer, Boolean> result = new HashMap<>();
+
+        if (portList == null) {
+            result.putAll(mDigitalInputValues);
+        }
+        else {
+            for (Integer wantedPort : portList) {
+                if (mDigitalInputValues.containsKey(wantedPort)) {
+                    result.put(wantedPort, mDigitalInputValues.get(wantedPort));
+                }
+            }
+        }
+        return result;
+    }
+
+    public synchronized double getAnalogInput(int port) {
+        if (!mAnalogInputValues.containsKey(port)) {
+            return 0.0;
+        }
+        return mAnalogInputValues.get(port);
+    }
+
+    public synchronized Map<Integer, Double> getAnalogInputMulti(List<Integer> portList) {
+        Map<Integer, Double> result = new HashMap<>();
+
+        if (portList == null) {
+            result.putAll(mAnalogInputValues);
+        }
+        else {
+            for (Integer wantedPort : portList) {
+                if (mAnalogInputValues.containsKey(wantedPort)) {
+                    result.put(wantedPort, mAnalogInputValues.get(wantedPort));
+                }
+            }
+        }
+        return result;
+    }
+
+    // =====================================================
+    // SUBCLASS-ACCESSIBLE SETTERS FOR DATA
+    // =====================================================
+    protected synchronized void setRobotDisabled(boolean disabled) {
+        mRobotDisabled = disabled;
+    }
+
+    protected synchronized void setRobotMode(RobotMode mode) {
+        mRobotMode = mode;
+    }
+
+    protected synchronized void setMatchInfo(MatchInfo info) {
+        mMatchInfo = info;
+    }
+
+    protected synchronized void setJoystickData(JoystickData[] data) {
+        mJoystickData = data;
+    }
+
+    // =====================================================
+    // SUBCLASS-ACCESSIBLE LISTENER NOTIFICATION METHODS
+    // =====================================================
+    protected void notifyDigitalInputsChanged(Map<Integer, Boolean> updates) {
+        Map<Integer, Boolean> updateCopy = new HashMap<>();
+        updateCopy.putAll(updates);
+
+        for (ILinkListener listener : mListeners) {
+            listener.onDigitalInputsChanged(updateCopy);
+        }
+    }
+
+    protected void notifyAnalogInputsChanged(Map<Integer, Double> updates) {
+        Map<Integer, Double> updateCopy = new HashMap<>();
+        updateCopy.putAll(updates);
+
+        for (ILinkListener listener : mListeners) {
+            listener.onAnalogInputsChanged(updates);
+        }
+    }
+
+    protected void notifyRobotDisabledChanged(boolean disabled) {
+        for (ILinkListener listener : mListeners) {
+            listener.onRobotDisabledChanged(disabled);
+        }
+    }
+
+    protected void notifyRobotModeChanged(RobotMode mode) {
+        for (ILinkListener listener : mListeners) {
+            listener.onRobotModeChanged(mode);
+        }
+    }
+
+    protected void notifyMatchInfoChanged(MatchInfo matchInfo) {
+        for (ILinkListener listener : mListeners) {
+            listener.onMatchInfoChanged(matchInfo);
+        }
+    }
+
+    protected void notifyJoystickDataChanged(JoystickData[] joystickData) {
+        for (ILinkListener listener : mListeners) {
+            listener.onJoystickDataChanged(joystickData);
+        }
+    }
+
+    // =====================================================
+    // SUBCLASS-IMPLEMENTED EVENT HANDLERS
+    // =====================================================
+    protected abstract void onDigitalOutputValuesChanged();
+    protected abstract void onPWMOutputValuesChanged();
+
 }
--- link_ros/src/main/java/com/ftlrobots/link/ros/RosLink.java
+++ link_ros/src/main/java/com/ftlrobots/link/ros/RosLink.java
@@ -28,29 +28,11 @@ public class RosLink extends FTLLink implements ISystemMessageListener {
     private boolean mLinkActive;
 
     // FTLLink properties
-    private boolean mRobotDisabled;
-    private RobotMode mRobotMode;
-    private MatchInfo mMatchInfo;
     private double mMatchTime;
-    private JoystickData[] mJoystickData;
-
-    private Map<Integer, Boolean> mDigitalOutputValues;
-    private Map<Integer, Double> mPWMOutputValues;
-    private Map<Integer, Boolean> mDigitalInputValues;
-    private Map<Integer, Double> mAnalogInputValues;
 
     public RosLink() {
         // === FTLLink Setup ===
-        mRobotDisabled = false;
-        mRobotMode = RobotMode.Autonomous;
-        mMatchInfo = null;
         mMatchTime = 0;
-        mJoystickData = null;
-
-        mDigitalOutputValues = new HashMap<>();
-        mPWMOutputValues = new HashMap<>();
-        mDigitalInputValues = new HashMap<>();
-        mAnalogInputValues = new HashMap<>();
 
         // === ROS Node Setup ===
         mNodeConfig = NodeConfiguration.newPrivate();
@@ -68,102 +50,18 @@ public class RosLink extends FTLLink implements ISystemMessageListener {
     }
 
     @Override
-    public boolean getRobotDisabled() {
-        return this.mRobotDisabled;
-    }
-
-    @Override
-    public RobotMode getRobotMode() {
-        return this.mRobotMode;
-    }
-
-    @Override
-    public MatchInfo getMatchInfo() {
-        return this.mMatchInfo;
-    }
-
-    @Override
     public double getMatchTime() {
-        return 0;
+        return mMatchTime;
     }
 
     @Override
-    public JoystickData[] getJoystickData() {
-        return this.mJoystickData;
+    protected void onDigitalOutputValuesChanged() {
+        // TODO Broadcast to ROS
     }
 
     @Override
-    public synchronized void setDigitalOutput(int port, boolean value) {
-        mDigitalOutputValues.put(port, value);
-    }
-
-    @Override
-    public synchronized void setDigitalOutput(Map<Integer, Boolean> digitalValueMap) {
-        for (Entry<Integer, Boolean> valEntry : digitalValueMap.entrySet()) {
-            mDigitalOutputValues.put(valEntry.getKey(), valEntry.getValue());
-        }
-    }
-
-    @Override
-    public synchronized void setPWMOutput(int port, double value) {
-        mPWMOutputValues.put(port, value);
-    }
-
-    @Override
-    public synchronized void setPWMOutput(Map<Integer, Double> pwmValueMap) {
-        for (Entry<Integer, Double> valEntry : pwmValueMap.entrySet()) {
-            mPWMOutputValues.put(valEntry.getKey(), valEntry.getValue());
-        }
-    }
-
-    @Override
-    public synchronized boolean getDigitalInput(int port) {
-        if (!mDigitalInputValues.containsKey(port)) {
-            return false;
-        }
-        return mDigitalInputValues.get(port);
-    }
-
-    @Override
-    public synchronized Map<Integer, Boolean> getDigitalInputMulti(List<Integer> portList) {
-        Map<Integer, Boolean> result = new HashMap<>();
-
-        if (portList == null) {
-            result.putAll(mDigitalInputValues);
-        }
-        else {
-            for (Integer wantedPort : portList) {
-                if (mDigitalInputValues.containsKey(wantedPort)) {
-                    result.put(wantedPort, mDigitalInputValues.get(wantedPort));
-                }
-            }
-        }
-        return result;
-    }
-
-    @Override
-    public synchronized double getAnalogInput(int port) {
-        if (!mAnalogInputValues.containsKey(port)) {
-            return 0.0;
-        }
-        return mAnalogInputValues.get(port);
-    }
-
-    @Override
-    public synchronized Map<Integer, Double> getAnalogInputMulti(List<Integer> portList) {
-        Map<Integer, Double> result = new HashMap<>();
-
-        if (portList == null) {
-            result.putAll(mAnalogInputValues);
-        }
-        else {
-            for (Integer wantedPort : portList) {
-                if (mAnalogInputValues.containsKey(wantedPort)) {
-                    result.put(wantedPort, mAnalogInputValues.get(wantedPort));
-                }
-            }
-        }
-        return result;
+    protected void onPWMOutputValuesChanged() {
+        // TODO Broadcast to ROS
     }
 
     // === ISystemMessageListener Implementation ===
@@ -173,6 +71,25 @@ public class RosLink extends FTLLink implements ISystemMessageListener {
         switch (msg.getSource()) {
             case "ftl-robot-mode": {
                 sLogger.log(Level.INFO, msg);
+                // TODO Grab the values for "disabled" and "mode"
+                if (msg.getValues().containsKey("disabled")) {
+                    notifyRobotDisabledChanged(msg.getValues().get("disabled"));
+                }
+                if (msg.getValues().containsKey("mode")) {
+                    RobotMode newMode = null;
+                    switch (msg.getValues().get("mode")) {
+                        case "auto":
+                            newMode = RobotMode.Autonomous;
+                            break;
+                        case "teleop":
+                            newMode = RobotMode.Teleop;
+                            break;
+                    }
+
+                    if (newMode != null) {
+                        notifyRobotModeChanged(newMode);
+                    }
+                }
             } break;
         }
     }
